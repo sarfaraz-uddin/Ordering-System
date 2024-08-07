cf951a8a1b2de9d3f7ca55aec8dbab3dd1f9273f
sarfaraz-uddin <sarfarazuddin7860@gmail.com>
2024-08-07 12:55:11 +0545

    contact page added

--- Ordering System/User/Contact.aspx.cs
+++ Ordering System/User/Contact.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,50 @@ namespace Ordering_System.User
 {
     public partial class Contact : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection(Connection.GetConnectionString());
+                cmd = new SqlCommand("ContactSp", con);
+                cmd.Parameters.AddWithValue("@Action", "INSERT");
+                cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim()));
+                cmd.Parameters.AddWithValue("@Subject", txtSubject.Text.Trim()));
+                cmd.Parameters.AddWithValue("@Message", txtMessage.Text.Trim()));
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                lblMsg.Visible = true;
+                lblMsg.Text = "Thanks for reaching out will look into your query";
+                lblMsg.CssClass = "alert alert-success";
+                clear();
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert(' " + ex.Message + " ');</script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void clear()
+        {
+            txtName.Text = string.Empty;
+            txtEmail.Text=string.Empty;
+            txtSubject.Text = string.Empty;
+            txtMessage.Text = string.Empty;
         }
     }
 }
